7b8bfab2f31f01fc21145ba439e57e55c01ebc96
lickey10 <lickey10@gmail.com>
2020-06-17 22:40:13 -0600

    updated unity

--- CarsWithGuns/Assets/PinwheelFantasyEffect/Script/Spin2.cs
+++ CarsWithGuns/Assets/PinwheelFantasyEffect/Script/Spin2.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Spin : MonoBehaviour {
+public class Spin2 : MonoBehaviour {
     public float turnDegPerSec = 360;
     public Vector3 localAxis;
     protected float turnDegPerFrame;
